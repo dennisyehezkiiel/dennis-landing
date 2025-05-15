bb53d34a8c1662931188920b9b7fee0645eb3b2f
dennismanullang <dennis.manullang@sprout.co.id>
2025-05-15 22:27:31 +0700

    split component

---
+++ src/app/landing/components/hero.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { poppins } from "@/app/layout";
+import { useRef } from "react";
+import Typewriter from "typewriter-effect";
+
+const Hero = () => {
+  const cardRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    const card = cardRef.current;
+    if (!card) return;
+
+    const { left, top, width, height } = card.getBoundingClientRect();
+    const x = (e.clientX - left - width / 2) / 20;
+    const y = (e.clientY - top - height / 2) / 20;
+
+    console.log(`rotateY(${x}deg) rotateX(${-y}deg)`, ">>cek ini");
+
+    card.style.transform = `rotateY(${x}deg) rotateX(${-y}deg)`;
+  };
+
+  const reset = () => {
+    if (cardRef.current) {
+      cardRef.current.style.transform = "rotateY(0deg) rotateX(0deg)";
+    }
+  };
+
+  return (
+    <div
+      onMouseMove={handleMouseMove}
+      onMouseLeave={reset}
+      className="flex flex-col items-center text-[90px] space-y-1"
+    >
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+            .typeString(`Hi, I'm Dennis!`)
+            .callFunction(() => {
+              console.log("String typed out!");
+            })
+            .pauseFor(2500)
+            .start();
+        }}
+      />
+      <div className="flex flex-col justify-center items-center space-y-4 w-fit">
+        <div
+          className={`flex text-center text-[18px] space-x-1 ${poppins.className}`}
+        >
+          <p>
+            i architect bugs into features & vibes into functions. deploy
+            dreams,{" "}
+          </p>
+          <p className="text-red-200">not drama.</p>
+        </div>
+        <div
+          ref={cardRef}
+          style={{ willChange: "transform" }}
+          className="bg-gray-600/30 rounded-full px-12 py-2 w-fit"
+        >
+          <p className={`text-[18px] ${poppins.className}`}>
+            Jakarta, Indonesia
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/app/landing/index.tsx
+++ src/app/landing/index.tsx
@@ -1,9 +1,10 @@
 "use client";
 import React, { useRef } from "react";
 import { Parallax, ParallaxLayer, IParallax } from "@react-spring/parallax";
-import Typewriter from "typewriter-effect";
 import { poppins, tiny5 } from "../layout";
 import Image from "next/image";
+import Hero from "./components/hero";
+import Projects from "./components/projects";
 
 // Little helpers ...
 const url = (name: string, wrap = false) =>
@@ -15,33 +16,9 @@ const url = (name: string, wrap = false) =>
 
 export default function Landing() {
   const parallax = useRef<IParallax>(null!);
-  const cardRef = useRef<HTMLDivElement>(null);
-
-  const handleMouseMove = (e: React.MouseEvent) => {
-    const card = cardRef.current;
-    if (!card) return;
-
-    const { left, top, width, height } = card.getBoundingClientRect();
-    const x = (e.clientX - left - width / 2) / 20;
-    const y = (e.clientY - top - height / 2) / 20;
-
-    console.log(`rotateY(${x}deg) rotateX(${-y}deg)`, ">>cek ini");
-
-    card.style.transform = `rotateY(${x}deg) rotateX(${-y}deg)`;
-  };
-
-  const reset = () => {
-    if (cardRef.current) {
-      cardRef.current.style.transform = "rotateY(0deg) rotateX(0deg)";
-    }
-  };
 
   return (
-    <div
-      onMouseMove={handleMouseMove}
-      onMouseLeave={reset}
-      style={{ width: "100%", height: "100%", background: "#253237" }}
-    >
+    <div style={{ width: "100%", height: "100%", background: "#253237" }}>
       <Parallax ref={parallax} pages={3}>
         <ParallaxLayer
           offset={1}
@@ -183,39 +160,7 @@ export default function Landing() {
             justifyContent: "center",
           }}
         >
-          <div className="flex flex-col items-center text-[90px] space-y-1">
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter
-                  .typeString(`Hi, I'm Dennis!`)
-                  .callFunction(() => {
-                    console.log("String typed out!");
-                  })
-                  .pauseFor(2500)
-                  .start();
-              }}
-            />
-            <div className="flex flex-col justify-center items-center space-y-4 w-fit">
-              <div
-                className={`flex text-center text-[18px] space-x-1 ${poppins.className}`}
-              >
-                <p>
-                  i architect bugs into features & vibes into functions. deploy
-                  dreams,{" "}
-                </p>
-                <p className="text-red-200">not drama.</p>
-              </div>
-              <div
-                ref={cardRef}
-                style={{ willChange: "transform" }}
-                className="bg-gray-600/30 rounded-full px-12 py-2 w-fit"
-              >
-                <p className={`text-[18px] ${poppins.className}`}>
-                  Jakarta, Indonesia
-                </p>
-              </div>
-            </div>
-          </div>
+          <Hero />
         </ParallaxLayer>
 
         <ParallaxLayer
@@ -228,126 +173,7 @@ export default function Landing() {
             justifyContent: "start",
           }}
         >
-          <div className="grid grid-cols-5 content-center gap-4">
-            <div className="grid-row-2 col-span-3 space-y-5 w-full overflow-hidden">
-              <div className="flex items-center overflow-hidden rounded-r-lg">
-                <div
-                  className="flex w-full space-x-8"
-                  style={{
-                    animation: "scroll-linear 20s linear infinite",
-                  }}
-                >
-                  {[...Array(99)].map((_, index) => (
-                    <div key={index} className="flex items-center space-x-8">
-                      <div className="relative rounded-lg bg-gradient-to-r from-white via-[#FBE8FF] to-[#FEF0FF] w-[430px] h-[200px]">
-                        <Image
-                          src={"/projects/alodokter-sgm.png"}
-                          alt="alodokter-sgm"
-                          fill
-                          objectFit="fill"
-                        />
-                      </div>
-                      <div className="relative rounded-lg bg-gradient-to-r from-white via-[#FBE8FF] to-[#FEF0FF] w-[340px] h-[200px]">
-                        <Image
-                          src={"/projects/cokran.jpg"}
-                          alt="alodokter-sgm"
-                          fill
-                          objectFit="cover"
-                          className="rounded-lg"
-                        />
-                      </div>
-                      <div className="relative rounded-lg bg-gradient-to-r from-white via-[#FBE8FF] to-[#FEF0FF] w-[430px] h-[200px]">
-                        <Image
-                          src={"/projects/sprout.jpg"}
-                          alt="alodokter-sgm"
-                          fill
-                          objectFit="cover"
-                          className="rounded-lg"
-                        />
-                      </div>
-                      <div className="relative rounded-lg bg-gradient-to-r from-white via-[#FBE8FF] to-[#FEF0FF] w-[350px] h-[200px]">
-                        <Image
-                          src={"/projects/toco.png"}
-                          alt="alodokter-sgm"
-                          fill
-                          objectFit="cover"
-                          className="rounded-lg"
-                        />
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <div className="flex items-center overflow-hidden rounded-r-lg">
-                <div
-                  className="flex w-full space-x-8"
-                  style={{
-                    animation: "scroll-linear-reverse 20s linear infinite",
-                  }}
-                >
-                  {[...Array(99)].map((_, index) => (
-                    <div key={index} className="flex items-center space-x-8">
-                      <div className="relative rounded-lg bg-gradient-to-r from-white via-[#FBE8FF] to-[#FEF0FF] w-[340px] h-[200px]">
-                        <Image
-                          src={"/projects/cokran.jpg"}
-                          alt="alodokter-sgm"
-                          fill
-                          objectFit="cover"
-                          className="rounded-lg"
-                        />
-                      </div>
-                      <div className="relative rounded-lg bg-gradient-to-r from-white via-[#FBE8FF] to-[#FEF0FF] w-[430px] h-[200px]">
-                        <Image
-                          src={"/projects/alodokter-sgm.png"}
-                          alt="alodokter-sgm"
-                          fill
-                          objectFit="fill"
-                        />
-                      </div>
-                      <div className="relative rounded-lg bg-gradient-to-r from-white via-[#FBE8FF] to-[#FEF0FF] w-[350px] h-[200px]">
-                        <Image
-                          src={"/projects/bouchon.png"}
-                          alt="alodokter-sgm"
-                          fill
-                          objectFit="cover"
-                          className="rounded-lg"
-                        />
-                      </div>
-                      <div className="relative rounded-lg bg-gradient-to-r from-white via-[#FBE8FF] to-[#FEF0FF] w-[350px] h-[200px]">
-                        <Image
-                          src={"/projects/toco.png"}
-                          alt="alodokter-sgm"
-                          fill
-                          objectFit="cover"
-                          className="rounded-lg"
-                        />
-                      </div>
-                      <div className="relative rounded-lg bg-gradient-to-r from-white via-[#FBE8FF] to-[#FEF0FF] w-[350px] h-[200px]">
-                        <Image
-                          src={"/projects/packet-pulse.png"}
-                          alt="alodokter-sgm"
-                          fill
-                          objectFit="cover"
-                          className="rounded-lg"
-                        />
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-            <div
-              className={`flex flex-col items-center justify-center col-span-2 w-full`}
-            >
-              <p className="text-[60px] text-white font-semibold">
-                Pixel alchemist
-              </p>
-              <p className={`${poppins.className} text-white`}>
-                turning Figma into fast, flawless web apps. 3 years React
-                wizardry, clean code addict, always shipping smooth vibes
-              </p>
-            </div>
-          </div>
+          <Projects />
         </ParallaxLayer>
 
         <ParallaxLayer
