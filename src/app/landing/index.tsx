5b55c17d0783c2a0a823681aaf3ce6661127832c
dennismanullang <dennis.manullang@sprout.co.id>
2025-05-20 16:35:57 +0700

    init assets

--- src/app/landing/index.tsx
+++ src/app/landing/index.tsx
@@ -49,7 +49,7 @@ export default function Landing() {
               }}
             >
               <div>
-                <img src={url("stars")} style={{ width: "100%" }} />
+              <img src={"/assets/stars.svg"} style={{ width: "100%" }} />
               </div>
             </ParallaxLayer>
           </div>
@@ -61,66 +61,66 @@ export default function Landing() {
           style={{ pointerEvents: "none" }}
         >
           <img
-            src={url("satellite4")}
+            src={"/assets/satellite4.svg"}
             style={{ width: "15%", marginLeft: "80%" }}
           />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
           <img
-            src={url("cloud")}
+            src={"/assets/cloud.svg"}
             style={{ display: "block", width: "20%", marginLeft: "55%" }}
           />
           <img
-            src={url("cloud")}
+            src={"/assets/cloud.svg"}
             style={{ display: "block", width: "10%", marginLeft: "15%" }}
           />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
           <img
-            src={url("cloud")}
+            src={"/assets/cloud.svg"}
             style={{ display: "block", width: "20%", marginLeft: "70%" }}
           />
           <img
-            src={url("cloud")}
+            src={"/assets/cloud.svg"}
             style={{ display: "block", width: "20%", marginLeft: "40%" }}
           />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
           <img
-            src={url("cloud")}
+            src={"/assets/cloud.svg"}
             style={{ display: "block", width: "10%", marginLeft: "10%" }}
           />
           <img
-            src={url("cloud")}
+            src={"/assets/cloud.svg"}
             style={{ display: "block", width: "20%", marginLeft: "75%" }}
           />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
           <img
-            src={url("cloud")}
+            src={"/assets/cloud.svg"}
             style={{ display: "block", width: "20%", marginLeft: "60%" }}
           />
           <img
-            src={url("cloud")}
+            src={"/assets/cloud.svg"}
             style={{ display: "block", width: "25%", marginLeft: "30%" }}
           />
           <img
-            src={url("cloud")}
+            src={"/assets/cloud.svg"}
             style={{ display: "block", width: "10%", marginLeft: "80%" }}
           />
         </ParallaxLayer>
 
         <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
           <img
-            src={url("cloud")}
+            src={"/assets/cloud.svg"}
             style={{ display: "block", width: "20%", marginLeft: "5%" }}
           />
           <img
-            src={url("cloud")}
+            src={"/assets/cloud.svg"}
             style={{ display: "block", width: "15%", marginLeft: "75%" }}
           />
         </ParallaxLayer>
@@ -135,17 +135,17 @@ export default function Landing() {
             pointerEvents: "none",
           }}
         >
-          <img src={url("earth")} style={{ width: "60%" }} />
+          <img src={"/assets/earth.svg"} style={{ width: "60%" }} />
         </ParallaxLayer>
 
         <ParallaxLayer
           offset={2}
           speed={-0.3}
-          style={{
-            backgroundSize: "80%",
-            backgroundPosition: "center",
-            backgroundImage: url("clients", true),
-          }}
+          // style={{
+          //   backgroundSize: "80%",
+          //   backgroundPosition: "center",
+          //   backgroundImage: url("clients", true),
+          // }}
         />
 
         <ParallaxLayer
@@ -174,7 +174,7 @@ export default function Landing() {
           <Projects />
         </ParallaxLayer>
 
-        <ParallaxLayer
+        {/* <ParallaxLayer
           offset={2}
           speed={-0}
           style={{
@@ -185,7 +185,7 @@ export default function Landing() {
           // onClick={() => parallax.current.scrollTo(0)}
         >
           <img src={url("clients-main")} style={{ width: "40%" }} />
-        </ParallaxLayer>
+        </ParallaxLayer> */}
       </Parallax>
     </div>
   );
